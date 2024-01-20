8dda5e8d524f69255bc5adf5feb3cb225f32d897
Jakub Świebocki <kuba.sw@interia.pl>
2024-01-20 21:15:56 +0100

    fix: Edit macros in STD_types

--- Std_Types.h
+++ Std_Types.h
@@ -12,7 +12,6 @@
 #include "Platform_Types.h"
 /* Normalnie byłoby też Compiler.h, ale dla uproszczenia nie jest wymagane w projekcie */
 
-/* [SWS_Std_00015] */
 typedef struct
 {
     uint16 vendorID;
@@ -22,26 +21,23 @@ typedef struct
     uint8 sw_patch_version;
 } Std_VersionInfoType;
 
-/* [SWS_Std_00005] */
 typedef uint8 Std_ReturnType;
 
-/* [SWS_Std_00006] */
 #ifndef STATUSTYPEDEFINED
 #define STATUSTYPEDEFINED
 #define E_OK        0x00U
 #endif
 #define E_NOT_OK    0x01U
 
-/* [SWS_Std_00007] */
 #define STD_HIGH     0x01U   /* Physical state 5V or 3.3V */
 #define STD_LOW      0x00U   /* Physical state 0V */
 
-/* [SWS_Std_00013] */
 #define STD_ACTIVE  0x01U    /* Logical state active */
 #define STD_IDLE    0x00U    /* Logical state idle */
 
-/* [SWS_Std_00010] */
 #define STD_ON      0x01U
 #define STD_OFF     0x00U
+#define NULL_PTR ((void *)0x00u)
+
 
 #endif /* STD_TYPES_H */
