303c777a8c4c57667dbfe560eb45eebf7fb36395
Jakub Świebocki <kuba.sw@interia.pl>
2024-01-21 18:15:12 +0100

    feat: CanTp_Init and CanTp_GetVersionInfo works, tests ready and passed

--- CanTp.c
+++ CanTp.c
@@ -12,28 +12,91 @@
 #include "CanTp.h"
 #include "CanIf.h"
 #include "PduR.h"
+
+/*====================================================================================================================*\
+    Makra lokalne
+\*====================================================================================================================*/
+
+
+/*====================================================================================================================*\
+    Typy lokalne
+\*====================================================================================================================*/
+typedef enum {
+    CANTP_ON, 
+    CANTP_OFF
+}CanTpState_type;
+
+typedef enum {
+    CANTP_RX_WAIT,
+    CANTP_RX_PROCESSING,
+    CANTP_RX_PROCESSING_SUSPEND
+} CanTpStateRX_type;
+
+typedef enum {
+    CANTP_TX_WAIT,                     
+    CANTP_TX_PROCESSING,                
+    CANTP_TX_PROCESSING_SUSPENDED
+} CanTpStateTX_type;
+
+typedef struct{
+    uint16 message_length; 
+    uint16 expected_CF_SN;              
+    uint16 sended_bytes;                
+    CanTpStateRX_type CanTp_StateRX;   
+    PduIdType CanTp_Current_RxId;      
+    uint8 blocks_to_next_cts; 
+} CanTp_VariablesRX_type;
+
+typedef struct{
+    CanTpStateTX_type CanTp_StateTX;
+    PduIdType CanTp_Current_TxId;
+    uint16 sent_bytes;
+    uint16 message_legth;
+    uint16 frame_nr_FC;  
+    uint8_t next_SN;
+} CanTp_VariablesTX_type;
+
+/*====================================================================================================================*\
+    Zmienne globalne
+\*====================================================================================================================*/
+CanTpState_type CanTp_State; 
+
+CanTp_VariablesTX_type CanTp_VariablesTX;
+
+CanTp_VariablesRX_type CanTp_VariablesRX;
+
+
+
 /*====================================================================================================================*\
     Deklaracje funkcji lokalnych
 \*====================================================================================================================*/
-static void CanTp_ResetRxState();
-static void CanTp_ResetTxState(void);
+static void CanTp_ResetRX(void);
+static void CanTp_ResetTX(void);
 
 /*====================================================================================================================*\
     Kod funkcji
 \*====================================================================================================================*/
 
+/*====================================================================================================================*/
 /**
   @brief CanTp_Init
 
- Funckja inicjalizuje moduł CanTp
-
+ Funckja inicjalizuje moduł CanTp [SWS_CANTP_00208]
 */
-void CanTp_Init ( void ){
-    CanTp_ResetRx();
-    CanTp_ResetTx();
-    CanTp_State = CAN_TP_ON;
+/*====================================================================================================================*/
+void CanTp_Init (void){
+    CanTp_ResetRX();
+    CanTp_ResetTX();
+    CanTp_State = CANTP_ON;
 }
+/*====================================================================================================================*/
+/*====================================================================================================================*/
+/**
+  @brief CanTp_GetVersionInfo
 
+ Funckja zwracajaca informacje o wersji [SWS_CANTP_00210]
+*/
+/*====================================================================================================================*/
 void CanTp_GetVersionInfo(Std_VersionInfoType *versioninfo) {
     if (versioninfo != NULL_PTR) {
     	versioninfo->sw_major_version = CANTP_SW_MAJOR_V;
@@ -44,11 +107,28 @@ void CanTp_GetVersionInfo(Std_VersionInfoType *versioninfo) {
     }
 }
 
-void CanTp_Shutdown(void) {
-    // if ((CanTp_StateType)CanTpState != (CanTp_StateType)CANTP_OFF) {
-    CanTpState = CANTP_OFF;
-    // }
-    // else {
-    //     CanTp_ReportError(0x00u, CANTP_SHUTDOWN, CANTP_E_UNINIT);
-    // }
+
+
+
+
+/*====================================================================================================================*\
+    Definicja funkcji lokalnych
+\*====================================================================================================================*/
+
+/*====================================================================================================================*/
+static void CanTp_ResetRX(void){
+    CanTp_VariablesRX.CanTp_StateRX = CANTP_RX_WAIT;
+    CanTp_VariablesRX.expected_CF_SN = 0;
+    CanTp_VariablesRX.message_length = 0;
+    CanTp_VariablesRX.sended_bytes = 0;
+    CanTp_VariablesRX.blocks_to_next_cts = 0;
+    CanTp_VariablesRX.CanTp_Current_RxId = 0;
+}
+
+static void CanTp_ResetTX(void){
+    CanTp_VariablesTX.sent_bytes = 0;
+    CanTp_VariablesTX.message_legth = 0;
+    CanTp_VariablesTX.CanTp_StateTX = CANTP_TX_WAIT;
+    CanTp_VariablesTX.frame_nr_FC = 0;
+    CanTp_VariablesTX.CanTp_Current_TxId = 0;
 }
--- UT_Can_TP.c
+++ UT_Can_TP.c
@@ -15,9 +15,38 @@
   Funkcja testująca inicjalizacji CanTp.
 */
 
-void Test_Of_CanTp_Init(void)
-{
-	
+void Test_Of_CanTp_Init(void){
+    CanTp_VariablesTX.CanTp_StateTX = CANTP_TX_WAIT;
+    CanTp_VariablesTX.frame_nr_FC = 1;
+    CanTp_VariablesTX.CanTp_Current_TxId = 0x1;
+    CanTp_VariablesTX.message_legth = 100;
+    CanTp_VariablesTX.sent_bytes = 95;
+    CanTp_VariablesTX.next_SN = 0;
+    CanTp_VariablesTX.CanTp_Current_TxId = 2;
+
+    CanTp_VariablesRX.CanTp_StateRX == CANTP_RX_PROCESSING;
+    CanTp_VariablesRX.blocks_to_next_cts = 1;
+    CanTp_VariablesRX.CanTp_Current_RxId = 1;
+    CanTp_VariablesRX.expected_CF_SN = 1;
+    CanTp_VariablesRX.sended_bytes = 1;
+
+    CanTp_State = CANTP_OFF;
+
+    CanTp_Init();
+    
+    TEST_CHECK(CanTp_VariablesTX.CanTp_StateTX == CANTP_TX_WAIT);
+    TEST_CHECK(CanTp_VariablesTX.CanTp_Current_TxId == 0); 
+    TEST_CHECK(CanTp_VariablesTX.frame_nr_FC == 0);
+    TEST_CHECK(CanTp_VariablesTX.message_legth == 0);
+    TEST_CHECK(CanTp_VariablesTX.sent_bytes == 0);
+
+    TEST_CHECK(CanTp_VariablesRX.CanTp_StateRX == CANTP_RX_WAIT);
+    TEST_CHECK(CanTp_VariablesRX.blocks_to_next_cts == 0);
+    TEST_CHECK(CanTp_VariablesRX.CanTp_Current_RxId == 0);
+    TEST_CHECK(CanTp_VariablesRX.expected_CF_SN == 0);
+    TEST_CHECK(CanTp_VariablesRX.sended_bytes == 0);
+
+    TEST_CHECK(CanTp_State == CANTP_ON);
 }
 
 /**
@@ -25,14 +54,14 @@ void Test_Of_CanTp_Init(void)
 
   Funkcja testująca wyłączenie CanTp.
 */
-void Test_Of_CanTp_Shutdown(void)
-{
-	CanTp_StateType CanTpState = CANTP_ON;
+// void Test_Of_CanTp_Shutdown(void)
+// {
+// 	CanTp_StateType CanTpState = CANTP_ON;
 
-	CanTp_Shutdown();
+// 	CanTp_Shutdown();
 
-    TEST_CHECK(CanTpState == CANTP_ON);
-}
+//     TEST_CHECK(CanTpState == CANTP_ON);
+// }
 
 /**
   @brief Test wersji
@@ -47,19 +76,18 @@ void Test_Of_CanTp_GetVersionInfo(void)
 	TEST_CHECK(versioninfo.sw_minor_version == 0x01u);
 	TEST_CHECK(versioninfo.sw_patch_version == 0x00u);
 	TEST_CHECK(versioninfo.vendorID == 0x00u);
-    TEST_CHECK(versioninfo.moduleID == 0x0Eu);
+  TEST_CHECK(versioninfo.moduleID == 0x0Eu);
 
 	TEST_CHECK(versioninfo.sw_major_version != 0x01u);
 	TEST_CHECK(versioninfo.sw_minor_version != 0x00u);
 	TEST_CHECK(versioninfo.sw_patch_version != 0x01u);
 	TEST_CHECK(versioninfo.vendorID != 0x01u);
-    TEST_CHECK(versioninfo.moduleID != 0x00u);
-
+  TEST_CHECK(versioninfo.moduleID != 0x00u);
 }
 
 TEST_LIST = {
-    // { "Test of CanTp_Init", Test_Of_CanTp_Init },
-    { "Test of CanTp_Shutdown", Test_Of_CanTp_Shutdown },
-	// { "Test of CanTp_GetVersionInfo", Test_Of_CanTp_GetVersionInfo },
+    { "Test of CanTp_Init", Test_Of_CanTp_Init },
+    // { "Test of CanTp_Shutdown", Test_Of_CanTp_Shutdown },
+	  { "Test of CanTp_GetVersionInfo", Test_Of_CanTp_GetVersionInfo },
     { NULL, NULL }                           
 };
