e152cb53b19eb3e36897d442bec03b8177224638
Jakub Świebocki <kuba.sw@interia.pl>
2024-01-21 19:30:33 +0100

    feat: CanTp_Transmit function added, test ready and passed

--- CanTp.c
+++ CanTp.c
@@ -56,6 +56,22 @@ typedef struct{
     uint8_t next_SN;
 } CanTp_VariablesTX_type;
 
+typedef enum {
+    SF = 0, // CANTP_N_PCI_TYPE_SF
+    FF = 1, // CANTP_N_PCI_TYPE_FF
+    CF = 2, // CANTP_N_PCI_TYPE_CF
+    FC = 3, // CANTP_N_PCI_TYPE_FC
+    DEFAULT = 4
+} frame_type_t;
+
+typedef struct{
+    frame_type_t frame_type;
+    uint32 frame_lenght; 
+    uint8 SN; // Sequence Nubmer
+    uint8 BS; // Block Size
+    uint8 FS; // Flow Status
+    uint8 ST; // Separation Time
+} CanPCI_Type;
 /*====================================================================================================================*\
     Zmienne globalne
 \*====================================================================================================================*/
@@ -73,6 +89,11 @@ CanTp_VariablesRX_type CanTp_VariablesRX;
 static void CanTp_ResetRX(void);
 static void CanTp_ResetTX(void);
 
+static Std_ReturnType CanTp_PrepareSegmenetedFrame(CanPCI_Type *CanPCI, PduInfoType *CanPdu_Info, uint8_t *Can_payload);
+
+static Std_ReturnType CanTp_SendSingleFrame(PduIdType id, uint8* payload, uint32 size);
+static Std_ReturnType CanTp_SendFirstFrame(PduIdType id, uint32 message_lenght);
+
 /*====================================================================================================================*\
     Kod funkcji
 \*====================================================================================================================*/
@@ -81,7 +102,7 @@ static void CanTp_ResetTX(void);
 /**
   @brief CanTp_Init
 
- Funckja inicjalizuje moduł CanTp [SWS_CANTP_00208]
+ Funkcja inicjalizuje moduł CanTp [SWS_CANTP_00208]
 */
 /*====================================================================================================================*/
 void CanTp_Init (void){
@@ -94,7 +115,7 @@ void CanTp_Init (void){
 /**
   @brief CanTp_GetVersionInfo
 
- Funckja zwracajaca informacje o wersji [SWS_CANTP_00210]
+ Funkcja zwracajaca informacje o wersji [SWS_CANTP_00210]
 */
 /*====================================================================================================================*/
 void CanTp_GetVersionInfo(Std_VersionInfoType *versioninfo) {
@@ -110,7 +131,11 @@ void CanTp_GetVersionInfo(Std_VersionInfoType *versioninfo) {
 /**
   @brief CanTp_Shutdown
 
-  Funckja wywoływana w celu wyłączenia modułu CanTp. [SWS_CANTP_00211]
+  Funkcja wywoływana w celu wyłączenia modułu CanTp. [SWS_CANTP_00211]
+
+  Wypełnia:
+   [SWS_CANTP_00202]
+   [SWS_CANTP_00200]
 */
 void CanTp_Shutdown(void){
     /* Wypełnia [SWS_CANTP_00202]*/
@@ -118,6 +143,78 @@ void CanTp_Shutdown(void){
     CanTp_ResetTX();
     /* Wypełnia [SWS_CANTP_00200]*/
     CanTp_State = CANTP_OFF;
+}
+
+/**
+  @brief CanTp_Transmit
+  Funkcja żąda przesłania PDU (Protocal Data Unit)  [SWS_CANTP_00212]
+  Wypełnia:
+   [SWS_CANTP_00231]
+   [SWS_CANTP_00232]
+   [SWS_CANTP_00204]
+   [SWS_CANTP_00205]
+   [SWS_CANTP_00206]
+   [SWS_CANTP_00298]
+   [SWS_CANTP_00299]
+   [SWS_CANTP_00321]
+   [SWS_CANTP_00354]
+*/
+
+Std_ReturnType CanTp_Transmit(PduIdType TxPduId, const PduInfoType* PduInfoPtr){
+
+    BufReq_ReturnType BufReq_State;
+    PduLengthType PDU_Len;
+    Std_ReturnType ret = E_OK;
+    
+    PduInfoType Temp_PDU;
+    uint8_t payload[8];
+    Temp_PDU.SduDataPtr = payload;
+    Temp_PDU.MetaDataPtr = NULL;
+    
+    if( CanTp_State == CANTP_ON ){
+        if( CanTp_VariablesTX.CanTp_StateTX == CANTP_TX_WAIT){
+            if(PduInfoPtr->SduLength < 8){
+                Temp_PDU.SduLength = PduInfoPtr->SduLength;
+                BufReq_State = PduR_CanTpCopyTxData(TxPduId, &Temp_PDU, NULL, &PDU_Len);
+                if(BufReq_State == BUFREQ_OK){
+                     /* Wypełnia [SWS_CANTP_00231]*/
+                     /* Wypełnia [SWS_CANTP_00354]*/
+                    ret = CanTp_SendSingleFrame(TxPduId, Temp_PDU.SduDataPtr, PduInfoPtr->SduLength );
+                }
+                /* Wypełnia [SWS_CANTP_00298]*/
+                else if(BufReq_State == BUFREQ_E_NOT_OK){
+                    CanTp_ResetTX();  
+                    /* Wypełnia [SWS_CANTP_00205]*/
+                    PduR_CanTpTxConfirmation(TxPduId, E_NOT_OK);
+                    ret = E_NOT_OK;
+                }
+                else {
+                    /* Wypełnia [SWS_CANTP_00204]*/
+                    // PduR_CanTpTxConfirmation(TxPduId, E_OK);
+                    ret = E_OK;
+                }
+            }
+            else{
+                if(CanTp_SendFirstFrame(TxPduId, PduInfoPtr->SduLength) == E_OK){
+                    CanTp_VariablesTX.CanTp_StateTX = TxPduId;
+                    CanTp_VariablesTX.CanTp_StateTX = CANTP_TX_PROCESSING_SUSPENDED;
+                    CanTp_VariablesTX.message_legth = PduInfoPtr->SduLength;
+                    CanTp_VariablesTX.sent_bytes = 0;
+                    ret = E_OK;
+                }
+                else{
+                    ret = E_NOT_OK;
+                }
+            }
+        }
+        else{
+            ret = E_NOT_OK;
+        }
+    }   
+    else{ 
+        ret = E_NOT_OK;
+    }
+    return ret;
 }
 
 
@@ -143,3 +240,137 @@ static void CanTp_ResetTX(void){
     CanTp_VariablesTX.frame_nr_FC = 0;
     CanTp_VariablesTX.CanTp_Current_TxId = 0;
 }
+
+static Std_ReturnType CanTp_PrepareSegmenetedFrame(CanPCI_Type *CanPCI, PduInfoType *CanPdu_Info, uint8_t *Can_payload){
+
+    Std_ReturnType ret = E_OK;
+
+    if(NE_NULL_PTR(CanPCI) && NE_NULL_PTR(CanPdu_Info) && NE_NULL_PTR(Can_payload)){  
+        
+        switch(CanPCI->frame_type){
+            case SF:
+                *(CanPdu_Info->SduDataPtr) = 0;
+                *(CanPdu_Info->SduDataPtr) = CANTP_N_PCI_TYPE_SF << 4;
+        
+                if(CanPCI->frame_lenght <= 7){
+                    *(CanPdu_Info->SduDataPtr) = 0x0F & CanPCI->frame_lenght; 
+                    for(uint8_t i = 0; i < CanPCI->frame_lenght; i++){
+                        *(CanPdu_Info->SduDataPtr + (i + 1)) = *(Can_payload + i);
+                    }  
+                }
+                else{
+                    ret = E_NOT_OK;
+                }
+            break;
+            case CF: 
+                *(CanPdu_Info->SduDataPtr) = 0;
+                *(CanPdu_Info->SduDataPtr) = CANTP_N_PCI_TYPE_CF << 4;
+                if(CanPCI->SN < 7){
+                    *(CanPdu_Info->SduDataPtr) |= (0x0F & CanPCI->SN);
+                    *(CanPdu_Info->SduDataPtr + 1) = *(Can_payload);
+                    *(CanPdu_Info->SduDataPtr + 2) = *(Can_payload + 1);
+                    *(CanPdu_Info->SduDataPtr + 3) = *(Can_payload + 2);
+                    *(CanPdu_Info->SduDataPtr + 4) = *(Can_payload + 3);
+                    *(CanPdu_Info->SduDataPtr + 5) = *(Can_payload + 4);
+                    *(CanPdu_Info->SduDataPtr + 6) = *(Can_payload + 5);
+                    *(CanPdu_Info->SduDataPtr + 7) = *(Can_payload + 6);
+                }
+                else{
+                    ret = E_NOT_OK; 
+                }
+
+            break;
+            case FF:    
+                *(CanPdu_Info->SduDataPtr) = 0;
+                *(CanPdu_Info->SduDataPtr) = CANTP_N_PCI_TYPE_FF << 4;
+
+                if(CanPCI->frame_lenght <= 4095){
+                    *(CanPdu_Info->SduDataPtr) |= (0x0F & (CanPCI->frame_lenght >> 8));
+                    *(CanPdu_Info->SduDataPtr + 1) = (0xFF & (CanPCI->frame_lenght));
+
+                    *(CanPdu_Info->SduDataPtr + 2) = *(Can_payload);
+                    *(CanPdu_Info->SduDataPtr + 3) = *(Can_payload + 1);
+                    *(CanPdu_Info->SduDataPtr + 4) = *(Can_payload + 2);
+                    *(CanPdu_Info->SduDataPtr + 5) = *(Can_payload + 3);
+                    *(CanPdu_Info->SduDataPtr + 6) = *(Can_payload + 4);
+                    *(CanPdu_Info->SduDataPtr + 7) = *(Can_payload + 5);
+                }
+                else{
+                    *(CanPdu_Info->SduDataPtr + 1) = 0;
+
+                    *(CanPdu_Info->SduDataPtr + 2) = (CanPCI->frame_lenght >> 24) & 0xFF;
+                    *(CanPdu_Info->SduDataPtr + 3) = (CanPCI->frame_lenght >> 16) & 0xFF;
+                    *(CanPdu_Info->SduDataPtr + 4) = (CanPCI->frame_lenght >> 8) & 0xFF;
+                    *(CanPdu_Info->SduDataPtr + 5) = (CanPCI->frame_lenght >> 0) & 0xFF;
+
+                    *(CanPdu_Info->SduDataPtr + 6) = *(Can_payload);
+                    *(CanPdu_Info->SduDataPtr + 7) = *(Can_payload + 1);
+                }
+            break;
+            case FC:
+                *(CanPdu_Info->SduDataPtr) = 0;
+                *(CanPdu_Info->SduDataPtr) = CANTP_N_PCI_TYPE_FC << 4;
+
+                if(CanPCI->FS < 7){
+                    *(CanPdu_Info->SduDataPtr) |= (0x0F & CanPCI->FS);
+                    *(CanPdu_Info->SduDataPtr + 1) = CanPCI->BS;
+                    *(CanPdu_Info->SduDataPtr + 2) = CanPCI->ST;
+                }
+                else{
+                    ret = E_NOT_OK;
+                }
+            break;
+            default:
+                ret = E_NOT_OK;
+            break;
+        }
+    }
+    else{
+        ret = E_NOT_OK;
+    }
+
+    return ret;
+}
+
+static Std_ReturnType CanTp_SendSingleFrame(PduIdType id, uint8* payload, uint32 size){
+    PduInfoType PduInfo;
+    uint8 SduDataPtr[8];
+    uint8 *MetaDataPtr;
+    PduInfo.MetaDataPtr = MetaDataPtr;
+    PduInfo.SduDataPtr = SduDataPtr;
+
+    CanPCI_Type CanPCI = {CANTP_N_PCI_TYPE_SF, size, 0, 0, 0, 0};
+    Std_ReturnType ret = E_OK;
+    ret = E_OK;
+    CanTp_PrepareSegmenetedFrame(&CanPCI, &PduInfo, payload);
+    
+    if(CanIf_Transmit(id , &PduInfo) == E_OK ){
+    }
+    else{
+        PduR_CanTpTxConfirmation(id, E_NOT_OK);
+        ret = E_NOT_OK;
+    }
+    return ret;
+}
+
+static Std_ReturnType CanTp_SendFirstFrame(PduIdType id, uint32 message_lenght){
+    PduInfoType PduInfo;
+    uint8 SduDataPtr[8];
+    uint8 *MetaDataPtr;
+    PduInfo.MetaDataPtr = MetaDataPtr;
+    PduInfo.SduDataPtr = SduDataPtr;
+
+    CanPCI_Type CanPCI = {CANTP_N_PCI_TYPE_FF, message_lenght, 0, 0, 0, 0}; 
+    uint8 payload[8] = {0,0,0,0,0,0,0,0};
+    Std_ReturnType ret = E_OK;
+
+    CanTp_PrepareSegmenetedFrame(&CanPCI, &PduInfo, payload);
+
+    if(CanIf_Transmit(id, &PduInfo) == E_OK ){}
+    else{
+        PduR_CanTpTxConfirmation(id, E_NOT_OK);
+        ret = E_NOT_OK;
+    }
+    return ret;
+}
+
--- UT_Can_TP.c
+++ UT_Can_TP.c
@@ -9,6 +9,32 @@
 
 #include "CanTp.c"   
 
+#include <stdio.h>
+#include <string.h>
+
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS; 
+
+FAKE_VALUE_FUNC(BufReq_ReturnType, PduR_CanTpCopyTxData, PduIdType, const PduInfoType*, const RetryInfoType*, PduLengthType*);
+FAKE_VOID_FUNC(PduR_CanTpTxConfirmation, PduIdType, Std_ReturnType );
+
+FAKE_VALUE_FUNC( Std_ReturnType, CanIf_Transmit, PduIdType, const PduInfoType* );
+
+uint8 PduR_CanTpCopyTxData_sdu_data[20][7];
+PduLengthType *PduR_CanTpCopyTxData_availableDataPtr; 
+
+BufReq_ReturnType PduR_CanTpCopyTxData_FF ( PduIdType id, const PduInfoType* info, const RetryInfoType* retry, PduLengthType* availableDataPtr ){
+    static int i = 0;
+    int loop_ctr;
+    i = PduR_CanTpCopyTxData_fake.call_count - 1;
+    for( loop_ctr = 0; loop_ctr < info->SduLength; loop_ctr++ ){
+      info->SduDataPtr[loop_ctr] = PduR_CanTpCopyTxData_sdu_data[i][loop_ctr];
+    }
+    *availableDataPtr = PduR_CanTpCopyTxData_availableDataPtr[i];
+    return PduR_CanTpCopyTxData_fake.return_val_seq[i];
+}
+
 /**
   @brief Test inicjalizacji
 
@@ -111,7 +137,116 @@ void Test_Of_CanTp_Shutdown(void){
     TEST_CHECK(CanTp_State != CANTP_ON);
 }
 
+/**
+  @brief Test żądania transmisji
+
+  Funkcja testująca żądanie transmisji PDU.
+*/
+void TestOf_CanTp_Transmit(void){
+
+  PduIdType PduId = 0x01;
+  PduInfoType PduInfo;
+  uint8 SduDataPtr[8];
+  uint8 *MetaDataPtr;
+  PduInfo.MetaDataPtr = MetaDataPtr;
+  PduInfo.SduDataPtr = SduDataPtr;
+  Std_ReturnType ret; 
+ 
+  PduLengthType availableDataPtr_array_local[10] = {1,2,3,4,5,6,7,8,9,0};
+  uint8 sdu_data_ptr_array[3][7] = { "dupa...", "dupa...", "test..2"};
+
+
+  PduLengthType availableData;
+  int i;
+
+  for( i = 0; i < 4; i++){
+      memcpy(PduR_CanTpCopyTxData_sdu_data[i], sdu_data_ptr_array[i], sizeof(uint8)*7);
+  }
+
+  PduR_CanTpCopyTxData_availableDataPtr = availableDataPtr_array_local;
+
+  RESET_FAKE( PduR_CanTpCopyTxData);
+  RESET_FAKE( CanIf_Transmit );
+  RESET_FAKE( PduR_CanTpTxConfirmation);
+
+  PduR_CanTpCopyTxData_fake.custom_fake = PduR_CanTpCopyTxData_FF;
+
+  Std_ReturnType CanIf_Transmit_retv[] = {E_OK, E_OK, E_NOT_OK, E_OK};
+  SET_RETURN_SEQ(CanIf_Transmit, CanIf_Transmit_retv, 4);
+
+  BufReq_ReturnType PduR_CanTpCopyTxData_retv[] = {BUFREQ_OK, BUFREQ_E_NOT_OK, BUFREQ_BUSY, BUFREQ_OK};
+  SET_RETURN_SEQ(PduR_CanTpCopyTxData, PduR_CanTpCopyTxData_retv, 4);
+
+  
+  /*                  TEST 1
+       CanTp_State = CANTP_OFF ---> Nic nie rób
+  */ 
+  CanTp_State = CANTP_OFF;
+  CanTp_VariablesTX.CanTp_StateTX = CANTP_TX_WAIT;
+  CanTp_VariablesTX.CanTp_Current_TxId = 1;
+  PduInfo.SduLength = 7;
+  
+  ret = CanTp_Transmit(PduId, &PduInfo);
+  
+  TEST_CHECK( ret == E_NOT_OK );
+  TEST_CHECK(CanTp_VariablesTX.CanTp_StateTX == CANTP_TX_WAIT);
+  TEST_CHECK(CanTp_VariablesTX.CanTp_Current_TxId == 1);
+ 
+ /*
+                                    TEST 2 
+  CANTP_TX_WAIT - SduLenght < 8 - wysyłane Single Frame - zwracane E_OK oraz BUFREQ_OK
+*/
+  CanTp_State = CANTP_ON;
+  CanTp_VariablesTX.CanTp_StateTX = CANTP_TX_WAIT;
+  CanTp_VariablesTX.CanTp_Current_TxId = 1;
+  PduInfo.SduLength = 7;
+
+  ret = CanTp_Transmit(PduId, &PduInfo);
+
+  TEST_CHECK(ret == E_OK);
+
+  TEST_CHECK(PduR_CanTpCopyTxData_fake.call_count == 1);
+
+  TEST_CHECK(PduR_CanTpCopyTxData_fake.arg0_val == PduId);
+  TEST_CHECK(PduR_CanTpCopyTxData_fake.arg1_val->SduLength == 7);
+  TEST_CHECK(PduR_CanTpCopyTxData_fake.arg2_val == NULL);
+ 
+  TEST_CHECK(CanIf_Transmit_fake.call_count == 1 );
+
+  TEST_CHECK(CanIf_Transmit_fake.arg0_val == 1 );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[0] == 0x7 );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[1] == 'd' );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[2] == 'u' );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[3] == 'p' );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[4] == 'a' );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[5] == '.' );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[6] == '.' );
+  TEST_CHECK(CanIf_Transmit_fake.arg1_val->SduDataPtr[7] == '.' );
+
+  TEST_CHECK(CanTp_VariablesTX.CanTp_StateTX == CANTP_TX_WAIT);
+  TEST_CHECK(CanTp_VariablesTX.CanTp_Current_TxId == 1);
+  
+  CanTp_ResetRX();
+
+/*
+                               TEST3 
+  CANTP_TX_WAIT - SduLenght < 8 -  zwracane E_NOT_OK oraz BUFREQ_E_NOT_OK
+*/
+  CanTp_VariablesTX.CanTp_StateTX = CANTP_TX_WAIT;
+  CanTp_VariablesTX.CanTp_Current_TxId = 1;
+  PduInfo.SduLength = 7;
+
+  ret = CanTp_Transmit(PduId, &PduInfo);
+  TEST_CHECK(ret == E_NOT_OK);
+  TEST_CHECK(PduR_CanTpCopyTxData_fake.call_count == 2);
+  TEST_CHECK(CanTp_VariablesTX.CanTp_StateTX == CANTP_TX_WAIT);
+  TEST_CHECK(CanTp_VariablesTX.CanTp_Current_TxId == 0);
+  TEST_CHECK(PduR_CanTpTxConfirmation_fake.call_count == 1);
+  TEST_CHECK(PduR_CanTpTxConfirmation_fake.arg1_val == E_NOT_OK);
+}
+
 TEST_LIST = {
+    { "TestOf_CanTp_Transmit", TestOf_CanTp_Transmit },
     { "Test of CanTp_Init", Test_Of_CanTp_Init },
     { "Test of CanTp_Shutdown", Test_Of_CanTp_Shutdown },
 	  { "Test of CanTp_GetVersionInfo", Test_Of_CanTp_GetVersionInfo },
